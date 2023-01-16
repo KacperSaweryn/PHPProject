5594c7c25ee26a1cbde44450312408303ef70ad9
KacperSaweryn <kacpersaweryn@gmail.com>
2023-01-16 17:55:10 +0100

    DB init, Login default page add

---
+++ Login.php
@@ -0,0 +1,19 @@
+<?php
+include('Functions.php');
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+
+</head>
+<body bgcolor=#add8e6 text="#000FFF">
+
+<hr><center>
+    <?php
+otworz_polaczenie();
+echo "Przychodnia"
+?>
+</center>
+</body>
+</html>
+
